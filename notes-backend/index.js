fc059e0e37a34310d5881f230f724f6dd2d0d42f
Jose Luis Gutierrez <joselugz03@gmail.com>
2023-06-07 22:07:18 -0300

    fix workspace

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import { PORT } from './config'
 
 const app = express()
 
@@ -74,5 +73,7 @@ app.put('/api/notes/:id', (request, response) => {
   response.json(notes)
 })
 
-app.listen(PORT)
-console.log(`Server running on port: ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${3001}`)
+})
